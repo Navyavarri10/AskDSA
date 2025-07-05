bc2b65ed45a506b9612d069fca6dcdfc156d479f
Navyavarri10 <navyadeeraj8@gmail.com>
2025-07-05 20:05:30 +0530

    Delete frontend directory

--- frontend/src/pages/accessToken.js
+++
@@ -1,6 +0,0 @@
-export function accessToken({ accessToken, userName, userId }) {
-  sessionStorage.setItem("accessToken", accessToken);
-  sessionStorage.setItem("userName", userName);
-  sessionStorage.setItem("userId", userId);
-  return;
-}
